d5477923be7693e902e915f068ec65ce15158f19
eshuerta <eshuerta@gmail.com>
2026-02-28 17:57:54 -0300

    force deploy

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -6,7 +6,7 @@ export const BRANCH_IMAGE_FOLDER = 'branches';
 export const DEFAULT_IMAGE_ORDER = 0;
 export const MAX_IMAGE_SIZE_MB = 5;
 
-export const PROPERTY_STATUS = {
+export const PROPERTY_STATUS = { 
   ACTIVE: 'active',
   INACTIVE: 'inactive',
   DRAFT: 'draft',
