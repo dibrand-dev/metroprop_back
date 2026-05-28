5df376c2ff928b61900ac80af74a9c840050aca6
eshuerta <eshuerta@gmail.com>
2026-05-28 12:44:22 -0300

    fix search get

--- src/modules/leads/leads.controller.ts
+++ src/modules/leads/leads.controller.ts
@@ -39,6 +39,36 @@ export class LeadsController {
     return this.leadsService.getLeadProperties({ email: user.email });
   }
 
+  // search leads by property_id and/or lead email
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.USER_ROL_ADMIN, UserRole.USER_ROL_SUPER_ADMIN)
+  @Get('search')
+  findFiltered(
+    @Query('property_id') propertyIdRaw: string | undefined,
+    @Query('email') email: string | undefined,
+    @Query('offset') offsetRaw: string | undefined,
+    @Query('limit') limitRaw: string | undefined,
+    @Req() request: Request,
+  ) {
+    const user = (request as any).user;
+    const filters: LeadFiltersDto = {};
+
+    if (propertyIdRaw !== undefined) filters.property_id = parseInt(propertyIdRaw, 10);
+    if (email) filters.email = email;
+    if (offsetRaw !== undefined) filters.offset = parseInt(offsetRaw, 10);
+    if (limitRaw !== undefined) filters.limit = parseInt(limitRaw, 10);
+
+    if (user.role_id === UserRole.USER_ROL_SUPER_ADMIN) {
+      // ve todo
+    } else if (user.role_id === UserRole.USER_ROL_ADMIN && user.organization_id !== undefined) {
+      filters.organization_id = user.organization_id;
+    } else {
+      filters.owner_user_id = user.id;
+    }
+
+    return this.leadsService.findAll(filters);
+  }
+
   @Get(':id')
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.USER_ROL_ADMIN, UserRole.USER_ROL_SUPER_ADMIN, UserRole.USER_ROL_SUPERVISOR)
@@ -82,33 +112,6 @@ export class LeadsController {
           return this.leadsService.remove(id);
     } 
     return false;
-  }
- 
-
-  // search leads by property_id and/or lead email
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(UserRole.USER_ROL_ADMIN, UserRole.USER_ROL_SUPER_ADMIN)
-  @Get('search')
-  findFiltered(
-    @Query('property_id') propertyIdRaw: string | undefined,
-    @Query('email') email: string | undefined,
-    @Req() request: Request,
-  ) {
-    const user = (request as any).user;
-    const filters: LeadFiltersDto = {};
-
-    if (propertyIdRaw !== undefined) filters.property_id = parseInt(propertyIdRaw, 10);
-    if (email) filters.email = email;
-
-    if (user.role_id === UserRole.USER_ROL_SUPER_ADMIN) {
-      // ve todo
-    } else if (user.role_id === UserRole.USER_ROL_ADMIN && user.organization_id !== undefined) {
-      filters.organization_id = user.organization_id;
-    } else {
-      filters.owner_user_id = user.id;
-    }
-
-    return this.leadsService.findAll(filters);
   }
 
 
